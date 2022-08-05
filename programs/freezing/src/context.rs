a8d81e980fd6861616cc9909747dc06d8706fe25
kkuzminykh <kkuzminykh@ggwp.world>
2022-08-05 12:02:47 +0500

    box it

--- programs/freezing/src/context.rs
+++ programs/freezing/src/context.rs
@@ -4,14 +4,12 @@ use anchor_lang::prelude::*;
 use anchor_lang::solana_program::program_option::COption;
 use anchor_spl::token::{Mint, Token, TokenAccount};
 
-// TODO: box it
-
 #[derive(Accounts)]
 pub struct Initialize<'info> {
     #[account(mut)]
     pub admin: Signer<'info>,
     #[account(init, payer = admin, space = FreezingParams::LEN)]
-    pub freezing_params: Account<'info, FreezingParams>,
+    pub freezing_params: Box<Account<'info, FreezingParams>>,
 
     /// CHECK: New mint auth PDA
     #[account(
@@ -24,23 +22,23 @@ pub struct Initialize<'info> {
     )]
     pub gpass_mint_auth: UncheckedAccount<'info>,
 
-    pub ggwp_token: Account<'info, Mint>,
+    pub ggwp_token: Box<Account<'info, Mint>>,
     #[account(mut,
         constraint = gpass_token.mint_authority == COption::Some(admin.key())
         @FreezingError::InvalidGPASSMintAuth
     )]
-    pub gpass_token: Account<'info, Mint>,
+    pub gpass_token: Box<Account<'info, Mint>>,
 
     #[account(
         constraint = accumulative_fund.mint == ggwp_token.key()
         @FreezingError::InvalidAccumulativeFundMint,
     )]
-    pub accumulative_fund: Account<'info, TokenAccount>,
+    pub accumulative_fund: Box<Account<'info, TokenAccount>>,
     #[account(
         constraint = treasury.mint == ggwp_token.key()
         @FreezingError::InvalidTreasuryMint,
     )]
-    pub treasury: Account<'info, TokenAccount>,
+    pub treasury: Box<Account<'info, TokenAccount>>,
 
     // Misc.
     pub system_program: Program<'info, System>,
@@ -91,14 +89,14 @@ pub struct Freeze<'info> {
         ],
         bump,
     )]
-    pub user_info: Account<'info, UserInfo>,
+    pub user_info: Box<Account<'info, UserInfo>>,
 
-    pub freezing_params: Account<'info, FreezingParams>,
+    pub freezing_params: Box<Account<'info, FreezingParams>>,
 
     #[account(mut,
         constraint = gpass_token.key() == freezing_params.gpass_token,
     )]
-    pub gpass_token: Account<'info, Mint>,
+    pub gpass_token: Box<Account<'info, Mint>>,
 
     #[account(mut,
         constraint = user_ggwp_wallet.mint == freezing_params.ggwp_token
@@ -106,20 +104,20 @@ pub struct Freeze<'info> {
         constraint = user_ggwp_wallet.owner == user.key()
         @FreezingError::InvalidUserGGWPWalletOwner,
     )]
-    pub user_ggwp_wallet: Account<'info, TokenAccount>,
+    pub user_ggwp_wallet: Box<Account<'info, TokenAccount>>,
     #[account(mut,
         constraint = user_gpass_wallet.mint == freezing_params.gpass_token
         @FreezingError::InvalidUserGPASSWalletMint,
         constraint = user_gpass_wallet.owner == user.key()
         @FreezingError::InvalidUserGPASSWalletOwner,
     )]
-    pub user_gpass_wallet: Account<'info, TokenAccount>,
+    pub user_gpass_wallet: Box<Account<'info, TokenAccount>>,
 
     #[account(mut,
         constraint = treasury.key() == freezing_params.treasury
         @FreezingError::InvalidTreasuryPK,
     )]
-    pub treasury: Account<'info, TokenAccount>,
+    pub treasury: Box<Account<'info, TokenAccount>>,
 
     /// CHECK: Mint auth PDA
     #[account(
@@ -149,14 +147,14 @@ pub struct Withdraw<'info> {
         ],
         bump,
     )]
-    pub user_info: Account<'info, UserInfo>,
+    pub user_info: Box<Account<'info, UserInfo>>,
 
-    pub freezing_params: Account<'info, FreezingParams>,
+    pub freezing_params: Box<Account<'info, FreezingParams>>,
 
     #[account(mut,
         constraint = gpass_token.key() == freezing_params.gpass_token,
     )]
-    pub gpass_token: Account<'info, Mint>,
+    pub gpass_token: Box<Account<'info, Mint>>,
 
     #[account(mut,
         constraint = user_gpass_wallet.mint == freezing_params.gpass_token
@@ -164,7 +162,7 @@ pub struct Withdraw<'info> {
         constraint = user_gpass_wallet.owner == user.key()
         @FreezingError::InvalidUserGPASSWalletOwner,
     )]
-    pub user_gpass_wallet: Account<'info, TokenAccount>,
+    pub user_gpass_wallet: Box<Account<'info, TokenAccount>>,
 
     /// CHECK: Mint auth PDA
     #[account(
@@ -193,14 +191,14 @@ pub struct Unfreeze<'info> {
         ],
         bump,
     )]
-    pub user_info: Account<'info, UserInfo>,
+    pub user_info: Box<Account<'info, UserInfo>>,
 
-    pub freezing_params: Account<'info, FreezingParams>,
+    pub freezing_params: Box<Account<'info, FreezingParams>>,
 
     #[account(mut,
         constraint = gpass_token.key() == freezing_params.gpass_token,
     )]
-    pub gpass_token: Account<'info, Mint>,
+    pub gpass_token: Box<Account<'info, Mint>>,
 
     #[account(mut,
         constraint = user_gpass_wallet.mint == freezing_params.gpass_token
@@ -208,7 +206,7 @@ pub struct Unfreeze<'info> {
         constraint = user_gpass_wallet.owner == user.key()
         @FreezingError::InvalidUserGPASSWalletOwner,
     )]
-    pub user_gpass_wallet: Account<'info, TokenAccount>,
+    pub user_gpass_wallet: Box<Account<'info, TokenAccount>>,
 
     /// CHECK: Mint auth PDA
     #[account(
